ac10fc870461210e9c5b4bd3f2ceaa337535c527
Jorge Zapata <cvem8165@gmail.com>
2025-03-27 14:38:34 -0500

    Add "Details" screen and integrate routing

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,12 +1,15 @@
 import { MemoryRouter, Routes, Route } from 'react-router';
 import Home from '../screens/Home/Home.tsx';
 import PageError from '../screens/PageError/PageError.tsx';
+import Details from '../screens/Details/Details.tsx';
+import { NAVIGATION } from '../constants/index.js';
 
 const MainNavigation = () => {
   return (
     <MemoryRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={NAVIGATION.home} element={<Home />} />
+        <Route path={NAVIGATION.details} element={<Details />} />
         <Route element={<PageError />} />
       </Routes>
     </MemoryRouter>
--- src/screens/Details/Details.tsx
+++ src/screens/Details/Details.tsx
@@ -1,14 +1,9 @@
 import { useCallback, useEffect, useState } from '@lynx-js/react';
 
-import './Home.css';
+import './Details.css';
 import arrow from '../../assets/arrow.png';
-import lynxLogo from '../../assets/lynx-logo.png';
-import reactLynxLogo from '../../assets/react-logo.png';
-import { useNavigate } from 'react-router';
-
-export default function Home() {
+export default function Details() {
   const [alterLogo, setAlterLogo] = useState(false);
-  const nav = useNavigate();
 
   useEffect(() => {
     console.info('Hello, ReactLynx');
@@ -17,26 +12,12 @@ export default function Home() {
   const onTap = useCallback(() => {
     'background only';
     setAlterLogo(!alterLogo);
-    setTimeout(() => {
-      nav('/details')
-    }, 1000)
   }, [alterLogo]);
 
   return (
     <view>
       <view className="Background" />
       <view className="App">
-        <view className="Banner">
-          <view className="Logo" bindtap={onTap}>
-            {alterLogo ? (
-              <image src={reactLynxLogo} className="Logo--react" />
-            ) : (
-              <image src={lynxLogo} className="Logo--lynx" />
-            )}
-          </view>
-          <text className="Title">Lynx</text>
-          <text className="Subtitle">Boilerplate</text>
-        </view>
         <view className="Content">
           <image src={arrow} className="Arrow" />
           <text className="Description">Tap the logo and have fun!</text>
